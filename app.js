0c02fb83398fe06b5ac0dae71bacc9bf5b70a79e
Ricky DeFazio <rickysdefazio@gmail.com>
2019-08-13 00:34:45 -0400

    Add bodyParser

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'pug');
 
