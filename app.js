a8cf7184e3388816533a2e953aa0c332a5b8f833
Ricky DeFazio <rickysdefazio@gmail.com>
2019-08-16 12:42:31 -0400

    Populate database with books

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ const { Book } = models;
 (async () => {
   try {
     // testing connection
+    console.log("Testing Connection...")
     await sequelize.authenticate();
     console.log("Connection succesful!");
     
@@ -66,7 +67,7 @@ const { Book } = models;
       Book.create({
         title: 'Ready Player One',
         author: 'Earnest Cline',
-        genre: 'Adventure',
+        genre: 'Science Fiction',
         year: 2011,
       }),
       Book.create({
@@ -81,9 +82,79 @@ const { Book } = models;
         genre: 'Autobiography',
         year: 2015,
       }),
+      Book.create({
+        title: 'Supermarket',
+        author: 'Bobby Hall',
+        genre: 'Fiction',
+        year: 2019,
+      }),
+      Book.create({
+        title: 'Smartcuts: How Hackers, Innovators, and Icons Accelerate Success',
+        author: 'Shane Snow',
+        genre: 'Business',
+        year: 2014,
+      }),
+      Book.create({
+        title: 'The Tipping Point: How Little Things Can Make a Big Difference',
+        author: 'Malcolm Gladwell',
+        genre: 'Business',
+        year: 2002,
+      }),
+      Book.create({
+        title: 'Rich Dad, Poor Dad',
+        author: 'Jordan Belfort',
+        genre: 'Business',
+        year: 2017,
+      }),
+      Book.create({
+        title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+        author: 'James Clear',
+        genre: 'Psychology',
+        year: 2018,
+      }),
+      Book.create({
+        title: '12 Rules for Life: An Antidote Chaos',
+        author: 'Jordan B. Peterson',
+        genre: 'Psychology',
+        year: 2018,
+      }),
+      Book.create({
+        title: 'Essentialism: The Disciplined Pursuit of Less',
+        author: 'Greg McKeown',
+        genre: 'Self-Development',
+        year: 2014,
+      }),
+      Book.create({
+        title: 'Flow: The Psychology of Optimal Experience',
+        author: 'Mihaly Csikszentmihalyi',
+        genre: 'Psychology',
+        year: 1990,
+      }),
+      Book.create({
+        title: 'Spark: The Revolutionary New Science of Exercise and the Brain',
+        author: 'John J. Ratey',
+        genre: 'Health',
+        year: 2008,
+      }),
+      Book.create({
+        title: 'Algorithms to Live By: The Computer Science of Human Decisions',
+        author: 'Brian Christian',
+        genre: 'Science',
+        year: 2016,
+      }),
+      Book.create({
+        title: 'Deep Work: Rules for Focused Success in a Distracted World',
+        author: 'Cal Newport',
+        genre: 'Self-Development',
+        year: 2016,
+      }),
+      Book.create({
+        title: 'The Righteous Mind: Why Good People Are Divided by Politics and Religion',
+        author: 'Jonathan Haidt',
+        genre: 'Psychology',
+        year: 2012,
+      }),
     ]);
-    
-    console.log(JSON.stringify(bookInstances, null, 2));
 
   } catch (error) {
     console.error('Error connecting to the database: ', error);
